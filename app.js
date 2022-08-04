06a9c08e0fb845eebc42243eb935aa733830d3cd
Farid ullah <faridullah996@gmail.com>
2022-08-04 15:20:39 +0500

    uncaught and unhandled exceptions handling.

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 require('express-async-errors');
 
+process.on('uncaughtException', (err) => {
+	console.log('Uncaught exception ->', err);
+	process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log('Unhandled reject ->', err);
+	process.exit(1);
+});
+
 const errorHandler = require('./middleware/error');
 
 const homeRouter = require('./routes/homeRoute');
