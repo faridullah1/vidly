5fea7e16adf3e309f7a164b6dfe21c74211ea4b7
Farid ullah <faridullah996@gmail.com>
2022-08-21 10:47:58 +0500

    setting up test db

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 module.exports = function() {
-	const databaseURL = process.env.DATABASE;
-	mongoose.connect(databaseURL)
-		.then(() => console.log('Connected to database'))
+	let db = process.env.DATABASE;
+	if (process.env.NODE_ENV === 'test') {
+		db = process.env.DATABASE_TEST;
+	}	
+	mongoose.connect(db)
+		.then(() => console.log(`Connected to ${db}`))
 		.catch(err => console.log('Could not connect to database...', err));
 }
