840d80fc17391bd135eaeb7d77447bfb5a5c3f2d
Farid ullah <faridullah996@gmail.com>
2022-09-23 18:04:03 +0500

    set home route to build

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const errorHandler = require('../middleware/error');
 
-const homeRouter = require('../routes/homeRoute');
 const genreRouter = require('../routes/genreRoutes');
 const customerRouter = require('../routes/customerRoutes');
 const movieRouter = require('../routes/movieRoutes');
@@ -14,7 +13,6 @@ module.exports = function(app) {
 	app.use(express.json());
 	app.use(express.urlencoded( { extended: true }));
 
-	app.use('/', homeRouter);
 	app.use('/api/v1/genres', genreRouter);
 	app.use('/api/v1/customers', customerRouter);
 	app.use('/api/v1/movies', movieRouter);
